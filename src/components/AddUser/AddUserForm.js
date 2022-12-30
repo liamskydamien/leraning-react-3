4d82a22d000c37aac4cefb274de53d6877f42e3c
Liam Hess <liamskydamien@gmail.com>
2022-12-30 13:33:46 +0100

    Adding refs

--- src/components/AddUser/AddUserForm.js
+++ src/components/AddUser/AddUserForm.js
@@ -1,58 +1,58 @@
-import React , { useState } from "react";
+import React , { useState , useRef} from "react";
 import styles from './AddUserForm.module.css';
 import Button from "../UI/Button";
 const AddUserForm = (props) => {
+    const nameInput = useRef();
+    const ageInput = useRef();
 
-    const [enteredName, setEnteredName] = useState('');
-    const [enteredAge, setEnteredAge] = useState('');
-
-
-    const cancelHandler = () => {
-        setEnteredName('');
-        setEnteredAge('');
-    }
 
     const addUserHandler = (event) => {
         event.preventDefault();
-        if (event.target.name.value === '' || event.target.age.value === '') {
+        const enteredNameValue = nameInput.current.value;
+        const enteredAgeValue = ageInput.current.value;
+
+        if (enteredNameValue === '' || enteredAgeValue === '') {
             props.error({name: 'Error', message: 'Please enter a name and age'});
         }
-        else if (event.target.age.value < 1) {
+        else if (enteredAgeValue < 1) {
             props.error({name: 'Error', message: 'Please enter a valid age'});
         }
         else {
             const user = {
-                name: enteredName,
-                age: enteredAge
+                name: enteredNameValue,
+                age: enteredAgeValue,
             }
             props.addUser(user);
-            setEnteredName('');
-            setEnteredAge('');
+            nameInput.current.value = '';
+            ageInput.current.value = '';
         }
     }
 
-    const nameChangeHandler = (name) => {
-            setEnteredName(name.target.value);
+    const resetHandler = () => {
+        nameInput.current.value = '';
+        ageInput.current.value = '';
     }
 
-    const ageChangeHandler = (age) => {
-        setEnteredAge(age.target.value);
-    }
 
     return <form onSubmit={addUserHandler}>
         <div className={styles.newUser__controls}>
             <div>
                 <label className={styles.newUser__control__label} htmlFor="username">Username</label>
-                <input className={styles.newUser__control__input} id="username" type="text" value={enteredName} onChange={nameChangeHandler}/>
+                <input className={styles.newUser__control__input}
+                       id="username" type="text"
+                       ref={nameInput}/>
             </div>
             <div>
                 <label className={styles.newUser__control__label} htmlFor="age">Age (Years)</label>
-                <input className={styles.newUser__control__input} id="age" type="number" value={enteredAge} onChange={ageChangeHandler}/>
+                <input className={styles.newUser__control__input}
+                       id="age"
+                       type="number"
+                       ref={ageInput}/>
             </div>
         </div>
         <div className={styles.newUser__actions}>
             <Button className="submit" type="submit" onClick={''}>Add User</Button>
-            <Button className="cancel" type="button" onClick={cancelHandler}>Cancel</Button>
+            <Button className="cancel" type="button" onClick={resetHandler}>Cancel</Button>
         </div>
     </form>
 }
