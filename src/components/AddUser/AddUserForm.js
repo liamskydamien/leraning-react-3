7b9ca12aace09bea4fec793f22a8ac6ff07fe1bc
Liam Hess <liamskydamien@gmail.com>
2022-12-30 13:34:04 +0100

    Adding refs

--- src/components/AddUser/AddUserForm.js
+++ src/components/AddUser/AddUserForm.js
@@ -1,4 +1,4 @@
-import React , { useState , useRef} from "react";
+import React , {useRef} from "react";
 import styles from './AddUserForm.module.css';
 import Button from "../UI/Button";
 const AddUserForm = (props) => {
